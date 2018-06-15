960589b15c0812d4877bd932f9e7a495b59d9ed1
Matt Roe <coachroe10@gmail.com>
2018-06-15 17:16:37 -0500

    checkpoint 4 list-messages-update: completed

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -4,27 +4,36 @@ class MessageList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
+      messages: [],
+      allMessages: []
     };
-    this.messagesRef = this.props.firebase.database().ref('messages');
+    this.messagesRef = this.props.firebase.database().ref('Messages');
   }
 
   componentDidMount() {
     this.messagesRef.on('child_added', snapshot => {
       const message = snapshot.val();
       message.key = snapshot.key;
-      this.setState( {messages: this.state.messages.concat(message)});
+      this.setState( {allMessages: this.state.allMessages.concat(message)});
+      this.updateDisplayMessages(this.props.activeRoom.key);
     });
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.updateDisplayMessages(nextProps.activeRoom.key)
+  }
+
+  updateDisplayMessages(messageRoom){
+    this.state.messages = this.state.allMessages.filter(message => message.roomId === messageRoom)
+  }
 
   render() {
     return (
       <div className = "messageList">
         <h1>Messages</h1>
         <div> {this.props.activeRoom.name} </div>
-        <div>{this.state.messages.filter(message => message.roomId === this.props.activeRoom.key).map((message, index) =>
-          <li key={index}>{message.content}</li>
+        <div>{this.state.messages.map((message, index) =>
+          <div key={index}>{message.content}</div>
           )}
         </div>
       </div>
